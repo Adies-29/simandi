b746f4f53e67a70083c75eee5e3827e8b3d5df8b
Adies <adiesadiansah6@gmail.com>
2026-04-06 20:24:33 +0700

    setup

---
+++ src/main/java/adies/simandi/Simandi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package adies.simandi;
+
+/**
+ *
+ * @author Adies
+ */
+public class Simandi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
